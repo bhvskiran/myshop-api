5377a9b022bd9d70f17ef7d1bd65007c397b57aa
bhvskiran <bhvskiran@gmail.com>
2023-06-06 18:52:55 +0530

    myshop backend initial commit consists of user apis

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use("/", express.static(path.join(__dirname, "./uploads")));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
+const user = require("./controller/user");
+app.use("/api/v2/user", user);
+
+//It's for errorhandling
+app.use(ErrorHandler);
+
+module.exports = app;
